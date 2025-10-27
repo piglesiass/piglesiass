8931e88631e7df86bae3dd7bd7a9662fc2907fba
Pablo <piglesiass03@iesalbarregas.es>
2025-10-27 17:28:29 +0100

    Update Ejercicio1_entdes.java

--- Ejercicio1_entdes.java
+++ Ejercicio1_entdes.java
@@ -1,5 +1,5 @@
 public class Ejercicio1_entdes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hoala cambio en remoto");
+        System.out.println("Hoala cambio en remoto. cambios");
 	}
 }
